4b5ac370c6e4957ff229910295eb69febdbd2355
Prithvi Anil K <thepirateking3d2y@gmail.com>
2021-02-27 20:11:12 +0530

    added socket-teacher <-> student interaction

--- client/src/components/Student/StudentView.tsx
+++ client/src/components/Student/StudentView.tsx
@@ -1,22 +1,47 @@
-import { Grid, Box, GridItem } from '@chakra-ui/react';
+import { Box, Grid, GridItem } from '@chakra-ui/react';
+import { useStoreState } from 'easy-peasy';
+import React, { useEffect, useState } from 'react';
+import { io, Socket } from 'socket.io-client';
+import { IAssignment } from '../../interfaces';
 import Editor from './Editor';
 import Questions from './Questions';
-import { io, Socket } from 'socket.io-client';
-
-import React, { useEffect } from 'react';
-import { useStoreState } from 'easy-peasy';
 
 let socket: Socket;
 const Student = () => {
     const { details } = useStoreState((store: any) => store.auth);
+    const [className, setClassName] = useState<string>('');
+    const [question, setQuestion] = useState<string>('');
+    const [inputDesc, setInputDesc] = useState<string>('');
+    const [outputDesc, setOutputDesc] = useState<string>('');
+    const [desc, setDesc] = useState<string>('');
+    const [testCases, setTestCases] = useState<
+        { input: string; output: string }[]
+    >([{ input: '', output: '' }]);
+
     useEffect(() => {
         const ENDPOINT = 'http://localhost:5000/';
         socket = io(ENDPOINT);
-        socket.emit('addStudent', details);
+        socket.emit('addStudent', details, (assignment: IAssignment) => {
+            const {
+                desc,
+                className,
+                inputDesc,
+                question,
+                outputDesc,
+                testCases,
+            } = assignment;
+            console.log(desc);
+            setQuestion(question);
+            setDesc(desc);
+            setClassName(className);
+            setInputDesc(inputDesc);
+            setOutputDesc(outputDesc);
+            setTestCases(testCases);
+        });
     }, []);
 
     return (
-        <Box className='amin-gradient'>
+        <Box className="amin-gradient">
             <Box padding="10px">
                 <Grid
                     h="100vh"
@@ -25,7 +50,14 @@ const Student = () => {
                     gap={4}
                 >
                     <GridItem rowSpan={10} colSpan={4}>
-                        <Questions />
+                        <Questions
+                            desc={desc}
+                            className={className}
+                            question={question}
+                            outputDesc={outputDesc}
+                            inputDesc={inputDesc}
+                            example={testCases[0]}
+                        />
                     </GridItem>
                     <GridItem colSpan={6} rowSpan={6}>
                         <Editor />

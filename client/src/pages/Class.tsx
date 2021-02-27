366cca010a1c9886e4ee8825f375889a252dbc0c
Prithvi Anil K <thepirateking3d2y@gmail.com>
2021-02-27 12:37:03 +0530

    added teacher form

--- client/src/pages/Class.tsx
+++ client/src/pages/Class.tsx
@@ -1,13 +1,26 @@
+import { Box } from '@chakra-ui/react';
 import { useStoreState } from 'easy-peasy';
 import React from 'react';
+import TeacherView from '../components/Teacher/TeacherView';
 
 interface ClassProps {}
 const Class: React.FC<ClassProps> = ({}) => {
     const { user, details } = useStoreState(
         (state: { auth: any }) => state.auth
     );
-
-    return <>{user}</>;
+    if (user === 'Teacher')
+        return (
+            <Box
+                display="flex"
+                flexDir="column"
+                justifyContent="center"
+                className="amin-gradient"
+                minH="100vh"
+            >
+                <TeacherView />
+            </Box>
+        );
+    else return <>student</>;
 };
 
 export default Class;
